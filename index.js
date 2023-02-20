6122fbb3ee3d32081a02f377f6da441447e8e524
Kauã <kauasampaio33@hotmail.com>
2023-02-20 16:13:50 -0300

    style: light dark com erros

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const mobilenav = document.getElementById('mobilenav')
 
+const html = document.querySelector('html')
+const checkbox = document.getElementById('switch-button')
+
+const logo = document.getElementById('img-header')
+
+checkbox.addEventListener('change', function(){
+
+  if(this.checked){
+    html.classList.toggle('dark-mode');
+    logo.style.backgroundImage = 'url(assets/logo-dark.png)'
+  }
+  else {
+    logo.style.backgroundImage = 'url(assets/logo-dark.png)'
+  }
+
+})
+
 
 function abrirMenu(){
   mobilenav.style.opacity = '1'
